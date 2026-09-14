3c34889b4d89c7d7720eb70f505fb93230d97156
YusakuKun <yusakukun@proton.me>
2026-09-15 01:37:34 +0200

    fix: omdøb pausemusik-knap til 'Emil i top 8' (#54)
    * fix: omdøb pausemusik-knap til 'Emil i top 8'
    
    * fix: omdøb pausemusik-knap til 'Emil i top 8'

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -485,8 +485,8 @@ export function Admin() {
             size="sm"
             className="border-2 border-ink bg-transparent text-ink hover:bg-ink hover:text-cream"
           >
-            <Link to="/pausemusik" title="🥚 Pausemusik">
-              <Music4 className="mr-1 h-4 w-4" aria-hidden="true" /> Pausemusik
+            <Link to="/pausemusik" title="🥚 Emil i top 8">
+              <Music4 className="mr-1 h-4 w-4" aria-hidden="true" /> Emil i top 8
             </Link>
           </Button>
         </div>
